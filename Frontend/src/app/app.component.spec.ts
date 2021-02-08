a062bdbeddbc43c5274935b3d2204c294f608fc7
marcinchaluda <chaluda@interia>
2021-02-08 11:20:25 +0100

    rename folder for frontend

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'FoodPin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FoodPin');
+    expect(app.title).toEqual('Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FoodPin app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Frontend app is running!');
   });
 });
