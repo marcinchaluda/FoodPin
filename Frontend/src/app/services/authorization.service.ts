1a562a53ca4ab5ed54c7e264295c447e0b956b3e
marcinchaluda <chaluda@interia>
2021-02-09 13:46:10 +0100

    implement login request and add Tokens model

---
+++ Frontend/src/app/services/authorization.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable, of} from "rxjs";
+import {environment} from "../../environments/environment";
+import {catchError, mapTo, tap} from "rxjs/operators";
+import {Tokens} from "../models/Tokens";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationService {
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
+  readonly apiUrl = environment.apiUrl;
+  private loggedUser: string;
+
+  constructor(private http: HttpClient) { }
+
+  login(user: {userName: string, password: string}): Observable<boolean> {
+    return this.http.post<any>(`${this.apiUrl}login`, user)
+      .pipe(
+        tap(tokens => this.doLoginUser(user.userName, tokens)),
+        mapTo(true),
+        catchError(error => {
+          alert(error.error);
+          return of(false);
+        })
+      );
+  }
+
+  private doLoginUser(userName: string, tokens: Tokens) {
+    this.loggedUser = userName;
+  }
+
+}
---
+++ Frontend/src/app/services/registration.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from '../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  readonly apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(userData): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}users`, userData);
+  }
+}
