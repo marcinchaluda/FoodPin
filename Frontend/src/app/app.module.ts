5cdf5a42bc313c8211a437d2634e640c95209031
marcinchaluda <chaluda@interia>
2021-02-15 13:54:42 +0100

    set up ngx toastr

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { HttpClientModule } from "@angular/common/http";
 import { LoginComponent } from './components/login/login.component';
 import { InputErrorPipe } from './pipes/input-error.pipe';
+import {ToastrModule} from "ngx-toastr";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -23,6 +25,8 @@ import { InputErrorPipe } from './pipes/input-error.pipe';
     ReactiveFormsModule,
     HttpClientModule,
     FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
