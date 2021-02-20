d70259267ae9c5b56b9a1bf1a4640150d43db169
marcinchaluda <chaluda@interia>
2021-02-20 14:41:48 +0100

    create layout and styles for loader

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {RegisterComponent} from "./components/register/register.component";
 import {LoginComponent} from "./components/login/login.component";
 import {HomeComponent} from "./components/home/home.component";
+import {LoaderComponent} from "./components/shared/loader/loader.component";
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'load', component: LoaderComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
--- Frontend/src/app/components/shared/loader/loader.component.ts
+++ Frontend/src/app/components/shared/loader/loader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faAppleAlt, faCarrot, faDrumstickBite, faIceCream, faPepperHot} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-loader',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
+  carrotIcon = faCarrot;
+  drumstickIcon = faDrumstickBite;
+  iceCreamIcon = faIceCream;
+  pepperIcon = faPepperHot;
+  appleIcon = faAppleAlt;
 
   constructor() { }
 
