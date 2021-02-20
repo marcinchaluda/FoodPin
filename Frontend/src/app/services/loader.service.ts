7b2289476ac1d293b0c5079d0ac437ebf8ce9158
marcinchaluda <chaluda@interia>
2021-02-20 14:51:26 +0100

    add show and hide functions for loader

--- Frontend/src/app/services/loader.service.ts
+++ Frontend/src/app/services/loader.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderService {
+  isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor() { }
+
+  public showLoader(): void {
+    this.isLoading.next(true);
+  }
+
+  public hideLoader(): void {
+    this.isLoading.next(false);
+  }
 }
