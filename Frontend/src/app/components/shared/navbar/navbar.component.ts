b29b4e51aaaa2f1c1d594c6abe62742bcdaffe4a
marcinchaluda <chaluda@interia>
2021-02-22 14:24:04 +0100

    add html layout to navbar

--- Frontend/src/app/components/shared/navbar/navbar.component.ts
+++ Frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {faEnvelope, faInfoCircle, faSignal, faUserCircle, faUserCog} from "@fortawesome/free-solid-svg-icons";
+import {faFacebookF, faInstagram, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  userIcon = faUserCircle;
+  accountIcon = faUserCog;
+  statsIcon = faSignal;
+  infoIcon = faInfoCircle;
+  facebookIcon = faFacebookF;
+  twitterIcon = faTwitter;
+  instagramIcon = faInstagram;
+  emailIcon = faEnvelope;
+  isOpen$ = true;
 
   constructor() { }
 
