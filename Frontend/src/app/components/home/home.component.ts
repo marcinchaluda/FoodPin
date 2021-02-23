fb9d60e3dbedfd1776518ec3ff8692f3663860a8
marcinchaluda <chaluda@interia>
2021-02-23 19:53:16 +0100

    move logout function to navbar component

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthorizationService} from "../../services/authorization.service";
 import {Router} from "@angular/router";
 import {ToastrService} from "ngx-toastr";
-import {faUserCircle, faEnvelope} from "@fortawesome/free-solid-svg-icons"
+import {faEnvelope, faUserCircle} from "@fortawesome/free-solid-svg-icons"
 import {faFacebookF, faInstagram, faTwitter} from "@fortawesome/free-brands-svg-icons"
-import {NavbarComponent} from "../shared/navbar/navbar.component";
 import {NavbarService} from "../../services/navbar.service";
 import {BehaviorSubject} from "rxjs";
 
@@ -23,23 +22,11 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private _authService: AuthorizationService,
-    private _router: Router,
-    private _toastr: ToastrService,
     private _navbar: NavbarService,
     ) { }
 
   public ngOnInit(): void {
     this.isOpen$ = this._navbar.isOpen$;
-  }
-
-  public logoutUser(): void {
-    if (!this._authService.getRefreshToken() !== null) {
-      this._authService.logout$().subscribe(
-        _ => {
-          this._toastr.success("Successfully logged out");
-        },
-      );
-    }
   }
 
   public toggleMenu() {
