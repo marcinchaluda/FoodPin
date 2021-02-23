eee51a5e44521deac623dd5a45bd632bc5cdc256
marcinchaluda <chaluda@interia>
2021-02-23 19:57:00 +0100

    implement logout function in navbar

--- Frontend/src/app/components/shared/navbar/navbar.component.ts
+++ Frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {faEnvelope, faInfoCircle, faSignal, faUserCircle, faUserCog} from "@fortawesome/free-solid-svg-icons";
 import {faFacebookF, faInstagram, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import {AuthorizationService} from "../../../services/authorization.service";
+import {BehaviorSubject} from "rxjs";
+import {ToastrService} from "ngx-toastr";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -17,12 +20,24 @@ export class NavbarComponent implements OnInit {
   twitterIcon = faTwitter;
   instagramIcon = faInstagram;
   emailIcon = faEnvelope;
-  isOpen$ = true;
-  loggedUser: string;
+  loggedUser$: BehaviorSubject<string>;
 
-  constructor(private _authService: AuthorizationService) { }
+  constructor(
+    private _authService: AuthorizationService,
+    private _toastr: ToastrService,
+    ) { }
 
   ngOnInit(): void {
-    this.loggedUser = this._authService.loggedUser;
+    this.loggedUser$ = this._authService.loggedUser$;
+  }
+
+  public logoutUser(): void {
+    if (!this._authService.getRefreshToken() !== null) {
+      this._authService.logout$().subscribe(
+        _ => {
+          this._toastr.success("Successfully logged out");
+        },
+      );
+    }
   }
 }
