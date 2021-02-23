f5e9aa9a4185a63c32f04c14bb289feb98e360f2
marcinchaluda <chaluda@interia>
2021-02-23 17:42:46 +0100

    substitute show adn hide functions with toggle navbar function

--- Frontend/src/app/services/navbar.service.ts
+++ Frontend/src/app/services/navbar.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavbarService {
+  isOpen$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  public toggleNavbar(): void {
+    this.isOpen$.next(!this.isOpen$.value);
+  }
 }
