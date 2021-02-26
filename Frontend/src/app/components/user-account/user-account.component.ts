3274c057f76761efe8128ae2e221440f2b768450
marcinchaluda <chaluda@interia>
2021-02-26 16:59:18 +0100

    create reactive form for user account

--- Frontend/src/app/components/user-account/user-account.component.ts
+++ Frontend/src/app/components/user-account/user-account.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {faEdit} from "@fortawesome/free-solid-svg-icons";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {patternValidator} from "../../validators/patternValidator.validator";
 
 @Component({
   selector: 'app-user-account',
@@ -10,10 +12,30 @@ export class UserAccountComponent implements OnInit {
   editIcon = faEdit;
   isDetailsActive = true;
   isAddressActive = true;
+  accountForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.accountForm = this.generateAccountForm();
   }
 
+  private generateAccountForm() {
+    return this._formBuilder.group({
+      username: [null , Validators.compose([Validators.minLength(6)])],
+      firstname: [null],
+      lastname: [null],
+      email: [null , Validators.compose([Validators.email])],
+      phone: [null , patternValidator(/^[+]*[(]?[0-9]{1,4}[)]?[-\s\./0-9]*$/, {invalidNumber: true})],
+      address: this._formBuilder.group({
+        street: [null],
+        localnumber: [null],
+        postalcode: [null, patternValidator(/\d{2}-\d{3}/, {invalidPostalCode: true})],
+        city: [null],
+        country: [null],
+      }),
+    });
+  }
 }
