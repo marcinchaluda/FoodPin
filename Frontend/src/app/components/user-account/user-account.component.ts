a3dfe4abe465196b3d63b487777428ae3e553593
marcinchaluda <chaluda@interia>
2021-02-27 13:59:53 +0100

    add reactive forms to html account and extract address form

--- Frontend/src/app/components/user-account/user-account.component.ts
+++ Frontend/src/app/components/user-account/user-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faEdit} from "@fortawesome/free-solid-svg-icons";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {patternValidator} from "../../validators/patternValidator.validator";
@@ -13,29 +13,39 @@ export class UserAccountComponent implements OnInit {
   isDetailsActive = true;
   isAddressActive = true;
   accountForm: FormGroup;
+  addressForm: FormGroup;
 
   constructor(
     private _formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.addressForm = this.generateAddressForm();
     this.accountForm = this.generateAccountForm();
   }
 
-  private generateAccountForm() {
+  private generateAccountForm(): FormGroup {
     return this._formBuilder.group({
       username: [null , Validators.compose([Validators.minLength(6)])],
       firstname: [null],
       lastname: [null],
       email: [null , Validators.compose([Validators.email])],
       phone: [null , patternValidator(/^[+]*[(]?[0-9]{1,4}[)]?[-\s\./0-9]*$/, {invalidNumber: true})],
-      address: this._formBuilder.group({
-        street: [null],
-        localnumber: [null],
-        postalcode: [null, patternValidator(/\d{2}-\d{3}/, {invalidPostalCode: true})],
-        city: [null],
-        country: [null],
-      }),
+      address: this.addressForm,
     });
+  }
+
+  private generateAddressForm(): FormGroup {
+    return this._formBuilder.group({
+      street: [null],
+      localnumber: [null],
+      postalcode: [null, patternValidator(/\d{2}-\d{3}/, {invalidPostalCode: true})],
+      city: [null],
+      country: [null],
+    });
+  }
+
+  public onSubmit(): void {
+    console.log('submitted');
   }
 }
--- Frontend/src/app/components/user-account/user-account.module.ts
+++ Frontend/src/app/components/user-account/user-account.module.ts
@@ -6,17 +6,19 @@ import {UserAccountComponent} from './user-account.component';
 import {HeaderComponent} from '../shared/header/header.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {LoaderService} from "../../services/loader.service";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     UserAccountComponent,
     HeaderComponent,
   ],
-  imports: [
-    CommonModule,
-    UserAccountRoutingModule,
-    FontAwesomeModule,
-  ],
+    imports: [
+        CommonModule,
+        UserAccountRoutingModule,
+        FontAwesomeModule,
+        ReactiveFormsModule,
+    ],
   providers: [LoaderService],
 })
 export class UserAccountModule { }
