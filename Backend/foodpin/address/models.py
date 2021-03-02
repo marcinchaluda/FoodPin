b1e7a168aa6b9edc59f5b002334c6b7c4146e69b
RobertMatlag <robert.matlag@gmail.com>
2021-03-02 15:19:34 +0100

    Fix bug: db crash during creating new user, it couldn't find address

--- Backend/foodpin/address/models.py
+++ Backend/foodpin/address/models.py
@@ -1,15 +1,29 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from users.models import Profile
 
 
 class Address(models.Model):
-    id = models.IntegerField(primary_key=True)
-    street = models.CharField(max_length=100)
-    local_number = models.IntegerField()
-    postal_code = models.CharField(max_length=30)
-    city = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    latitude = models.DecimalField(max_digits=14, decimal_places=10)
-    longitude = models.DecimalField(max_digits=14, decimal_places=10)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    street = models.CharField(max_length=100, null=True)
+    local_number = models.IntegerField(default=1, null=True)
+    postal_code = models.CharField(max_length=30, null=True)
+    city = models.CharField(max_length=50, null=True)
+    country = models.CharField(max_length=50, null=True)
+    latitude = models.DecimalField(max_digits=14, decimal_places=10, null=True)
+    longitude = models.DecimalField(max_digits=14, decimal_places=10, null=True)
 
     def __str__(self):
         return f'{self.street} {self.local_number}, {self.postal_code} {self.city}'
+
+
+@receiver(post_save, sender=Profile)
+def create_user_address(sender, instance, created, **kwargs):
+    if created:
+        Address.objects.create(profile=instance)
+
+
+@receiver(post_save, sender=Profile)
+def save_user_address(sender, instance, **kwargs):
+    instance.address.save()
--- Backend/foodpin/users/models.py
+++ Backend/foodpin/users/models.py
@@ -1,8 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from address.models import Address
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class CustomUser(AbstractUser):
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
