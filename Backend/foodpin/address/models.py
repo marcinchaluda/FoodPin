8811b18fb8f984bb5a3dfcf94f01bd59a5fac833
RobertMatlag <robert.matlag@gmail.com>
2021-03-03 14:32:59 +0100

    Add default values

--- Backend/foodpin/address/models.py
+++ Backend/foodpin/address/models.py
@@ -6,13 +6,13 @@ from users.models import Profile
 
 class Address(models.Model):
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    street = models.CharField(max_length=100, null=True)
-    local_number = models.IntegerField(default=1, null=True)
-    postal_code = models.CharField(max_length=30, null=True)
-    city = models.CharField(max_length=50, null=True)
-    country = models.CharField(max_length=50, null=True)
-    latitude = models.DecimalField(max_digits=14, decimal_places=10, null=True)
-    longitude = models.DecimalField(max_digits=14, decimal_places=10, null=True)
+    street = models.CharField(max_length=100, default='')
+    local_number = models.IntegerField(default=0)
+    postal_code = models.CharField(max_length=30, default='')
+    city = models.CharField(max_length=50, default='')
+    country = models.CharField(max_length=50, default='')
+    latitude = models.DecimalField(max_digits=14, decimal_places=10, default=0.0)
+    longitude = models.DecimalField(max_digits=14, decimal_places=10, default=0.0)
 
     def __str__(self):
         return f'{self.street} {self.local_number}, {self.postal_code} {self.city}'
