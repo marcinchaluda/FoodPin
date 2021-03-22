0246fcf34ac82b5e50aac20dcce08bf0325e23ad
marcinchaluda <chaluda@interia>
2021-03-22 11:29:19 +0100

    create most waste products empty component

---
+++ Frontend/src/app/shared/most-waste-products/most-waste-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-most-waste-products',
+  templateUrl: './most-waste-products.component.html',
+  styleUrls: ['./most-waste-products.component.scss']
+})
+export class MostWasteProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
