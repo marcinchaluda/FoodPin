93000ba90aa4c6c8ad1e2369c8fa417d8ad30803
marcinchaluda <chaluda@interia>
2021-03-24 14:35:24 +0100

    add header and navbar to donation component

--- Frontend/src/app/components/donations/donations.component.ts
+++ Frontend/src/app/components/donations/donations.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {NavbarService} from '../../shared/navbar/navbar.service';
 
 @Component({
   selector: 'app-donations',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donations.component.scss']
 })
 export class DonationsComponent implements OnInit {
+  isOpen$: BehaviorSubject<boolean>;
 
-  constructor() { }
+  constructor(
+    private _navbarService: NavbarService,
+  ) { }
 
   ngOnInit(): void {
+    this.isOpen$ = this._navbarService.isOpen$;
   }
 
 }
